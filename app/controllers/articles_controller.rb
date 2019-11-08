06d09ab761e59f35ec522210be47e1a92fdc4d34
Walter Andrag <walter.andrag@prodigyfinance.com>
2019-11-08 13:29:52 +0200

    Update, delete articles + flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,33 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+
+    byebug
+    flash.notice = "Article '#{article.title}' Deleted!"
+
+    redirect_to articles_path
   end
 
   # def article_params
